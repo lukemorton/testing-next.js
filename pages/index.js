c1ba6d4bb17bfb28fa7b941798ee28ae50977d3d
Luke Morton <lukemorton.dev@gmail.com>
2016-11-25 17:36:38 +0000

    Introduce Welcome

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Welcome from '../components/welcome'
 import Articles from '../components/articles'
 
 const articles = [{ title: 'A great article',
@@ -7,6 +8,8 @@ const articles = [{ title: 'A great article',
 
 export default () =>
   <Layout title="Continuous Delivery">
+    <Welcome shoutout={<h1>Improving software delivery in every organisation</h1>} />
+
     <Articles title={<h2>Latest Articles</h2>}
               articles={articles} />
   </Layout>
