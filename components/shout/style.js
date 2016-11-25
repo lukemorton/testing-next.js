c383771d66b2a3dd719012f4736eeeb3a10838d3
Luke Morton <lukemorton.dev@gmail.com>
2016-11-25 17:52:23 +0000

    Rename Welcome to Shout and add style

---
+++ components/shout/style.js
@@ -0,0 +1,8 @@
+import css from 'next/css'
+
+const shout = css({
+  fontSize: '1.2em',
+  textAlign: 'center'
+})
+
+export default { shout }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
-import Welcome from '../components/welcome'
+import Shout from '../components/shout'
 import Articles from '../components/articles'
 
 const articles = [{ title: 'A great article',
@@ -8,7 +8,12 @@ const articles = [{ title: 'A great article',
 
 export default () =>
   <Layout title="Continuous Delivery">
-    <Welcome shoutout={<h1>Improving software delivery in every organisation</h1>} />
+    <Shout>
+      <h1>
+        Improving software delivery<br />
+        in every organisation
+      </h1>
+    </Shout>
 
     <Articles title={<h2>Latest Articles</h2>}
               articles={articles} />
