82b1c19f0ec0823d9a66e85bbea049682081b3d5
Luke Morton <lukemorton.dev@gmail.com>
2016-11-25 17:36:30 +0000

    Rename articleData to articles

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Layout from '../components/layout'
 import Articles from '../components/articles'
 
-const articleData = [{ title: 'Article title',
-                       href: '/blog/2016-11-25/great-article' }]
+const articles = [{ title: 'A great article',
+                    href: '/blog/2016-11-25/great-article' }]
 
 export default () =>
   <Layout title="Continuous Delivery">
     <Articles title={<h2>Latest Articles</h2>}
-              articles={articleData} />
+              articles={articles} />
   </Layout>
