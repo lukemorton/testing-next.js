f4054244ea8a7d34c7e068fdbccac99c42dfa0a7
Luke Morton <lukemorton.dev@gmail.com>
2016-11-27 00:39:51 +0100

    Add test for gallery images

--- test/pages/indexTest.js
+++ test/pages/indexTest.js
@@ -2,10 +2,21 @@ import test from 'ava'
 import React from 'react'
 import { shallow } from 'enzyme'
 import Shout from '../../components/Shout'
+import Gallery from '../../components/Gallery'
 import index from '../../pages'
 
 test('index contains shoutout', t => {
   const page = shallow(index())
   t.is(page.find(Shout).length, 1)
   t.true(page.find(Shout).contains('Improving software delivery in every organisation'))
+})
+
+test('index contains gallery', t => {
+  const page = shallow(index())
+  t.is(page.find(Gallery).length, 1)
+  t.true(page.find(Gallery).contains([
+    <img src='/static/office/team.jpg' />,
+    <img src='/static/office/laptop.jpg' />,
+    <img src='/static/office/crows.jpg' />
+  ]))
 })
