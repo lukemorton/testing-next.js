f6ecaadd949c6e52469b35bd50e8f67a23343b0e
Luke Morton <lukemorton.dev@gmail.com>
2016-11-27 22:31:03 +0100

    Add navigation to header

--- components/Header/index.js
+++ components/Header/index.js
@@ -5,8 +5,15 @@ import style from './style'
 export default () =>
   <header className={style.header}>
     <Link href='/'>
-      <img
-        src='/static/logo.png'
-        className={style.logo} />
+      <a className={style.logo}>
+        <img src='/static/logo.png' />
+      </a>
     </Link>
+
+    <nav className={style.nav}>
+      <Link href='/'>software development</Link>
+      <Link href='/'>continuous delivery</Link>
+      <Link href='/articles'>our writings</Link>
+      <Link href='/'>careers</Link>
+    </nav>
   </header>
--- components/Header/style.js
+++ components/Header/style.js
@@ -1,14 +1,30 @@
 import style from 'next/css'
 
 const header = style({
-  display: 'block',
-  paddingTop: '4em',
-  paddingBottom: '1em',
-  textAlign: 'center'
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  paddingTop: '2em',
+  paddingBottom: '4em',
+  textAlign: 'center',
+  '@media (min-width: 50em)': {
+    margin: '0 auto',
+    width: '50em'
+  }
 })
 
 const logo = style({
-  marginLeft: '-.5em'
+  display: 'block',
+  marginLeft: '-.5em',
+  '& > img': {
+    display: 'block'
+  }
 })
 
-export default { header, logo }
+const nav = style({
+  '& > a': {
+    paddingLeft: '1em'
+  }
+})
+
+export default { header, logo, nav }
