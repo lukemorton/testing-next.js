89f7b0928564e9672b1075319df4b5f12b767482
Luke Morton <lukemorton.dev@gmail.com>
2016-11-27 22:31:38 +0100

    Constrain articles index

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Page from '../../components/Page'
+import Constrained from '../../components/Constrained'
 import Articles from '../../components/Articles'
 import articles from '../../data/articles'
 
 export default () =>
   <Page title='Writings of Made Tech and friends'>
-    <Articles
-      title={<h2>Writings of Made Tech and friends</h2>}
-      articles={articles} />
+    <Constrained>
+      <Articles
+        title={<h1>Our Writings</h1>}
+        articles={articles} />
+    </Constrained>
   </Page>
